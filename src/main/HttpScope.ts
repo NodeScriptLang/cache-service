c6e17e63508ec42717ea4aa0611d530e29444072
inca <boris@okunskiy.name>
2024-10-24 15:18:29 +0300

    chore: add MainHttpHandler

--- src/main/HttpScope.ts
+++ src/main/HttpScope.ts
@@ -5,6 +5,7 @@ import { CacheDomainImpl } from './scoped/CacheDomainImpl.js';
 import { CacheProtocolHandler } from './scoped/CacheProtocolHandler.js';
 import { CacheProtocolImpl } from './scoped/CacheProtocolImpl.js';
 import { HttpAuthHandler } from './scoped/HttpAuthHandler.js';
+import { MainHttpHandler } from './scoped/MainHttpHandler.js';
 import { NodeScriptApi } from './scoped/NodeScriptApi.js';
 
 export class HttpScope extends Mesh {
@@ -16,6 +17,7 @@ export class HttpScope extends Mesh {
         this.service(CacheProtocolImpl);
         this.service(CacheProtocolHandler);
         this.service(HttpAuthHandler);
+        this.service(MainHttpHandler);
         this.service(NodeScriptApi);
     }
 
